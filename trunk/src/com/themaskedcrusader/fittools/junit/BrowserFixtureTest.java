a0cbee1a9b67a08be1de117ffd4d4d6fd6282aa2
christopher.schalk <christopher.schalk@821068a0-cc40-11dd-a412-a1792178251d>
2008-12-17 15:22:53 +0000

    Initial Check in.  Coding for this project will be done in Oracle JDeveloper.

---
+++ trunk/src/com/themaskedcrusader/fittools/junit/BrowserFixtureTest.java
@@ -0,0 +1,13 @@
+package com.themaskedcrusader.fittools.junit;
+
+import com.themaskedcrusader.fittools.fixture.BrowserFixture;
+import junit.framework.TestCase;
+
+public class BrowserFixtureTest extends TestCase {
+
+    public void testBrowserFixture() {
+        BrowserFixture bf = new BrowserFixture();
+//        bf.start();
+    }
+
+}
