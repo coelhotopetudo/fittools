0b5e64acd41d8c545ba93817d3ca6a2792c5650b
christopher.schalk <christopher.schalk@821068a0-cc40-11dd-a412-a1792178251d>
2008-12-18 13:26:55 +0000

    Added fixtures and JavaDoc to StringFixture.java.  Not complete, but should checkin before I lose my data

--- trunk/src/com/themaskedcrusader/fittools/fixture/StringFixture.java
+++ trunk/src/com/themaskedcrusader/fittools/fixture/StringFixture.java
@@ -18,6 +18,62 @@ public class StringFixture extends BaseDoFixture {
     super.utils.setGlobal(global, toReturn);
     return toReturn;
   }
+  
+  public void storeStringInGlobal(String toStore, String global) {
+    toStore = parse(toStore);
+    utils.setGlobal(global,toStore.trim());
+  }
+  
+  public void storeTokenOfStringInGlobal (int token, String string, String global) {
+    string = parse(string);
+    String[] tokens = string.split(" ");
+    utils.setGlobal(global, tokens[token]);
+  }
+  
+  public void printGlobal(String global) {
+    System.out.println(utils.getGlobal(global));
+  }
+
+  public void trimCharactersFromBeginningOfGlobal(int characters, String global) {
+    String toTrim = utils.getGlobal(global);
+    toTrim = toTrim.substring(characters);
+    utils.setGlobal(global,toTrim);
+  }
+  
+  public void trimCharactersFromEndOfGlobal(int characters, String global) {
+    String toTrim = utils.getGlobal(global);
+    toTrim = toTrim.substring(0,toTrim.length() - characters);
+    utils.setGlobal(global,toTrim);
+  }
+  
+  public void concatenateWithAndStoreInGlobal(String c1, String c2, String global){
+    c1 = parse(c1).trim();
+    c2 = parse(c2).trim();
+    c1 = c1 + c2;
+    utils.setGlobal(global,c1);
+  }
+  
+  public void storeStringLengthInGlobal(String toGet, String global) {
+    toGet = parse(toGet);
+    utils.setGlobal(global,"" + toGet.trim().length());
+  }
+  
+  public void compareWith(String c1, String c2) {
+    c1 = parse(c1);
+    c2 = parse(c2);
+    System.out.println(c1 + " : " + c2);                  
+  }
+  
+
+/**
+ * 
+ * Remove all occurances of character from string
+ *    removeCharacterFromString
+ *    removeCharacterFromStringAll
+ *
+ * 
+ * 
+ */
 
 
 }
