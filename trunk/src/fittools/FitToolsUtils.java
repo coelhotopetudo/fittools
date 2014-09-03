dd6b9c17829bb98a47e7b8ecdbfac3fbe7bdb727
christopher.schalk <christopher.schalk@821068a0-cc40-11dd-a412-a1792178251d>
2014-09-03 12:20:22 +0000

    Fixed License - Apache 2.0.

--- trunk/src/fittools/FitToolsUtils.java
+++ trunk/src/fittools/FitToolsUtils.java
@@ -1,19 +1,17 @@
 /* FitTools: FitNesse Plugin for Automation of Web Applications
- * Copyright (C) 2009-2012, Christopher Schalk (www.themaskedcrusader.com)
+ * Copyright (C) 2009-2014, Christopher Schalk (www.themaskedcrusader.com)
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  * Contact the author via email at: christopher.schalk@gmail.com
  */
@@ -37,21 +35,7 @@ public class FitToolsUtils {
     private int count = 0;
     private static FitToolsUtils thisInstance = new FitToolsUtils();
 
-    private FitToolsUtils() {
-        System.out.println(
-                "----- GPL License Notice: ------------------------------------------\n" +
-                "FitTools version 0.1, Copyright (C) 2009-2012 Christopher Schalk  \n" +
-                "FitTools comes with ABSOLUTELY NO WARRANTY; for details      \n" +
-                "add the following code to a fitnesse test page and execute:  \n\n" +
-                "!| fittools.fixture.BaseDoFixture |\n" +
-                "| print gpl warranty |\n\n" +
-                "FitTools is free software, and you are welcome to redistribute\n" +
-                "it under certain conditions; add the following code to a \n" +
-                "fitnesse test page and execute for details on condititions:\n\n" +
-                "!|fittools.fixture.BaseDoFixture |\n" +
-                "| print redistribution conditions |\n\n" +
-                "--------------------------------------------------------------------\n");
-    }
+    private FitToolsUtils() {}
 
     public static FitToolsUtils getInstance() {
         return thisInstance;
